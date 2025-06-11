b4742bafca9001c2e76d731445d5d378daad981d
Varun Krishna P <varunkrishna.dbz@gmail.com>
2025-06-11 13:53:31 +0000

    more code and .gitignore file added to ignore the my_nlp_env

--- 1.py
+++ 1.py
@@ -1,6 +1,17 @@
 from nltk.book import *
+import matplotlib.pyplot as plt
 
 # searching text
 
-print("concordance is used to view the every occurence of the given string together with some context.....")
+print(">>>concordance is used to view the every occurence of the given string together with some context.....>>>")
 text1.concordance("monstrous")
+
+print(">>>To see similar words within the same range use 'similar' and the word in paranthesis...>>>")
+text1.similar("monstrous")
+
+print(">>> common_contexts allows to examine the contexts that are shared by two or more words, enclose the words in a [].... >>>")
+
+text2.common_contexts(["monstrous", "very"])
+
+text4.dispersion_plot(["citizens", "democracy", "freedom", "duties", "America"])
+plt.show()
