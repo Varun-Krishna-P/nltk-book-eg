db46d335bea061db1bd3ed7dce29162c24a11028
Varun Krishna P <varunkrishna.dbz@gmail.com>
2025-06-11 13:29:34 +0000

    initial commit

---
+++ 1.py
@@ -0,0 +1,6 @@
+from nltk.book import *
+
+# searching text
+
+print("concordance is used to view the every occurence of the given string together with some context.....")
+text1.concordance("monstrous")
